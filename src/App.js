8f20664c58a30124eb5aa1d5b6c1c0ba4cb21ab2
aavetisyanit <aavetisyanit@gmail.com>
2021-02-09 11:53:27 -0600

    Install SASS, create and style homepage component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component';
 
 function App() {
-	return <div>HELLO</div>;
+	return <HomePage />;
 }
 
 export default App;
