507a1ef67da951522349778fd2dabde5f073574a
aavetisyanit <aavetisyanit@gmail.com>
2021-02-11 11:11:05 -0600

    Update routes to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-
-import { connect } from 'react-redux';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
+
+import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import './App.css';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends Component {
 	unsubscribeFromAuth = null;
@@ -43,15 +43,29 @@ class App extends Component {
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route path='/shop' component={ShopPage} />
-					<Route path='/signin' component={SignInAndSignUp} />
+					<Route
+						exact
+						path='/signin'
+						render={() =>
+							this.props.currentUser ? (
+								<Redirect to='/' />
+							) : (
+								<SignInAndSignUp />
+							)
+						}
+					/>
 				</Switch>
 			</div>
 		);
 	}
 }
 
+const maStateToProps = ({ user }) => ({
+	currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = dispact => ({
 	setCurrentUser: user => dispact(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(maStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { auth } from './../../firebase/firebase.utils';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
-
-import './header.styles.scss';
 import { connect } from 'react-redux';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import './header.styles.scss';
 
 function Header({ currentUser }) {
 	return (
