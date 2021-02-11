e73349b61cc7b90b5a5bb40b0f0ec884bca2951d
aavetisyanit <aavetisyanit@gmail.com>
2021-02-11 11:59:48 -0600

    Implement cart-reducer and toggle functionality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,24 @@
 import './cart-icon.styles.scss';
 
+import { connect } from 'react-redux';
+import CartActionTypes from './../../redux/cart/cart.types';
+import { toggleCartHidden } from './../../redux/cart/cart.actions';
+
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import React from 'react';
 
-export default function CartIcon() {
+function CartIcon({ toggleCartHidden }) {
 	return (
-		<div className='cart-icon'>
+		<div className='cart-icon' onClick={toggleCartHidden}>
 			<ShoppingIcon className='shopping-icon' />
 			<span className='item-count'>0</span>
 		</div>
 	);
 }
+
+const mapDispatchToProps = dispatch => ({
+	toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
+
+export default connect(null, mapDispatchToProps)(CartIcon);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import './header.styles.scss';
 import CartIcon from './../cart-icon/cart-icon.component';
 import CartDropdown from './../cart-dropdown/cart-dropdown.component';
 
-function Header({ currentUser }) {
+function Header({ currentUser, hidden }) {
 	return (
 		<div className='header'>
 			<Link className='logo-container' to='/'>
@@ -33,13 +33,14 @@ function Header({ currentUser }) {
 				)}
 				<CartIcon />
 			</div>
-			<CartDropdown />
+			{hidden ? null : <CartDropdown />}
 		</div>
 	);
 }
 
-const mapStateToProps = state => ({
-	currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+	currentUser,
+	hidden,
 });
 
 export default connect(mapStateToProps)(Header);
