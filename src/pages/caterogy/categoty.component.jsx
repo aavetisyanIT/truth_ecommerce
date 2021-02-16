18978131f0b4d1d94d046eba741e33a17b3578cd
aavetisyanit <aavetisyanit@gmail.com>
2021-02-15 21:21:09 -0600

    Create Category page with dynamical routes

---
+++ src/pages/caterogy/categoty.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './category.styles.scss';
+
+export default function CategoryPage({ match }) {
+	return (
+		<div className='category'>
+			<h2>CATEGORY PAGE</h2>
+		</div>
+	);
+}
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { Route } from 'react-router-dom';
+
+import CategoryPage from './../caterogy/categoty.component';
 import CollectionOverview from './../../components/collections-overview/collection-overview.component';
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
 	<div className='shop-page'>
-		<CollectionOverview />
+		<Route exact path={`${match.path}`} component={CollectionOverview} />
+		<Route path={`${match.path}/:categoryId`} component={CategoryPage} />
 	</div>
 );
 
