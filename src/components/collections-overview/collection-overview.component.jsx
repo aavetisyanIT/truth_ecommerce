e2c3a1c7fe3efaa465c7066f27f40371f02881bf
aavetisyanit <aavetisyanit@gmail.com>
2021-02-16 22:44:39 -0600

    Make new collection selector to map collections to array for collection-preview component

--- src/components/collections-overview/collection-overview.component.jsx
+++ src/components/collections-overview/collection-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { selectCollections } from './../../redux/shop/shop.selectors';
+import { SelectCollectionsForPreview } from './../../redux/shop/shop.selectors';
 
 import CollectionPreview from './../collection-review/collection-preview.component';
 
@@ -19,7 +19,7 @@ function CollectionOverview({ collections }) {
 }
 
 const mapStateToProps = createStructuredSelector({
-	collections: selectCollections,
+	collections: SelectCollectionsForPreview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
