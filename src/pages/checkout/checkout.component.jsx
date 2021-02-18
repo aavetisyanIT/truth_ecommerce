96e288b4ba1d898c01649ec3ea0376a4d515a5a2
aavetisyanit <aavetisyanit@gmail.com>
2021-02-17 22:24:43 -0600

    Create, set up and style stripe-checkout component.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
 import {
 	selectCartItems,
 	selectCartTotal,
 } from './../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
-import { connect } from 'react-redux';
 import CheckoutItem from './../../components/checkout-item/checkout-item.component';
+import StripeButton from './../../components/stripe-button/stripe-button.component';
 
 function CheckoutPage({ cartItems, total }) {
 	return (
@@ -40,6 +42,12 @@ function CheckoutPage({ cartItems, total }) {
 			<div className='total'>
 				<span>TOTAL: ${total}</span>
 			</div>
+			<div className='test-warning'>
+				*Please use the following test credit card for payment
+				<br />
+				4242 4242 4242 4242 - Exp: 01/22 -CVV: 123
+			</div>
+			<StripeButton price={total} />
 		</div>
 	);
 }
