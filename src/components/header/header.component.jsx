8d687cedb76ab7cca35d16d89b61ccefba235f09
aavetisyanit <aavetisyanit@gmail.com>
2021-02-18 21:39:22 -0600

    Convert homepage and header component to jsx

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,43 +1,43 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import { auth } from './../../firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import './header.styles.scss';
 import CartIcon from './../cart-icon/cart-icon.component';
 import CartDropdown from './../cart-dropdown/cart-dropdown.component';
 import { selectCurrentUser } from './../../redux/user/user.selectors';
 import { selectCartHidden } from './../../redux/cart/cart.selectors';
 
+import {
+	HeaderContainer,
+	LogoContainer,
+	OptionDiv,
+	OptionsContainer,
+	OptionLink,
+} from './header.styles';
+
 function Header({ currentUser, hidden }) {
 	return (
-		<div className='header'>
-			<Link className='logo-container' to='/'>
+		<HeaderContainer>
+			<LogoContainer to='/'>
 				<Logo className='logo' />
-			</Link>
-			<div className='options'>
-				<Link className='option' to='/shop'>
-					SHOP
-				</Link>
-				<Link className='option' to='/shop'>
-					CONTENT
-				</Link>
+			</LogoContainer>
+			<OptionsContainer>
+				<OptionLink to='/shop'>SHOP</OptionLink>
+				<OptionLink to='/shop'>CONTENT</OptionLink>
 				{currentUser ? (
-					<div className='option' onClick={() => auth.signOut()}>
+					<OptionDiv onClick={() => auth.signOut()}>
 						SIGN OUT
-					</div>
+					</OptionDiv>
 				) : (
-					<Link className='option' to='/signin'>
-						SIGN IN
-					</Link>
+					<OptionLink to='/signin'>SIGN IN</OptionLink>
 				)}
 				<CartIcon />
-			</div>
+			</OptionsContainer>
 			{hidden ? null : <CartDropdown />}
-		</div>
+		</HeaderContainer>
 	);
 }
 
