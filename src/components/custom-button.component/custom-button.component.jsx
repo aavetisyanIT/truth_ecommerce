a2fb4ef1f4edc03af59f0ac1ff97ffde09f2df53
aavetisyanit <aavetisyanit@gmail.com>
2021-02-27 20:03:52 -0600

    Separat app into client folder, add express server to handle backend stripe payments

--- src/components/custom-button.component/custom-button.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { CustomButtonContainer } from './custom-button.styles';
-
-export default function CustomButton({ children, ...props }) {
-	return (
-		<CustomButtonContainer {...props}>{children}</CustomButtonContainer>
-	);
-}
